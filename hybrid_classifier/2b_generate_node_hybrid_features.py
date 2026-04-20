7340e8d1f8a63bc7a8378a08002f5f1575502173
spicyvicks <eh202201077@wmsu.edu.ph>
2026-04-20 11:35:43 +0800

    fix: Restore neutral class (13 classes) - models were trained with it

--- hybrid_classifier/2b_generate_node_hybrid_features.py
+++ hybrid_classifier/2b_generate_node_hybrid_features.py
@@ -27,8 +27,8 @@ CLASS_NAMES = [
     'left_eye_thrust_correct', 'left_knee_block_correct', 'left_temple_block_correct',
     'right_chest_thrust_correct', 'right_elbow_block_correct',
     'right_eye_thrust_correct', 'right_knee_block_correct', 'right_temple_block_correct',
-    'solar_plexus_thrust_correct'
-    # Note: 'neutral' removed to match TuroArnis app (12 classes)
+    'solar_plexus_thrust_correct',
+    'neutral'
 ]
 
 # Skeleton edges
